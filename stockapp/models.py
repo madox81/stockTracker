da382fbac11e10e08189c6f7ce962d1098f58974
madox81 <m256a@yahoo.com>
2024-10-24 00:48:30 +0300

    Adding code to model and correct index.html

--- stockapp/models.py
+++ stockapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+# Create StockData model
+class StockData(models.Model):
+    ticker = models.CharField(max_length=10)
+    date = models.DateField()
+    open_price = models.DecimalField(max_digits=10, decimal_places=2)
+    close_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.ticker} ({self.date})"
